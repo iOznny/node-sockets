2c34baa323ce8707e1c3b75859e6b4beeb049a4c
Cesar Candia <cesarcandia@iMac-de-Cesar.local>
2021-06-23 17:57:50 -0500

    Cambio

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,1 +1,20 @@
-console.log('dddd');
+const socket = io();
+
+// Referencias html
+const lOnline = document.querySelector('#lOnline');
+const lOffline = document.querySelector('#lOffline');
+
+socket.on('connect', () => {
+    console.log('Conn');
+
+    lOffline.style.display = 'none';
+    lOnline.style.display = '';
+});
+
+
+socket.on('disconnect', () => {
+    console.log('Discnn');
+
+    lOnline.style.display = 'none';
+    lOffline.style.display = '';
+});
