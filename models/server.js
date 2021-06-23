b1ebd6c50221e111d7da102a0f24fbc97e72fd1b
Cesar Candia <cesarcandia@iMac-de-Cesar.local>
2021-06-23 17:21:13 -0500

    Cambio

--- models/server.js
+++ models/server.js
@@ -5,6 +5,8 @@ class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
+        this.server = require('http').createServer(this.app);
+        this.io = require('socket.io')(this.server);
 
         // Routes Path
         this.paths = {}
@@ -29,7 +31,7 @@ class Server {
     }
 
     listen() {
-        this.app.listen(this.port, () => {
+        this.server.listen(this.port, () => {
             console.log('Servidor corriendo en puerto', this.port);
         });
     }
