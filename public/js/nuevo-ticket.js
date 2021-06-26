c6fb1a4467c6a67c89feee8dd90fa265b1a78f8e
iOznny <f000170014@gmail.com>
2021-06-26 17:14:40 -0500

    c

--- public/js/nuevo-ticket.js
+++ public/js/nuevo-ticket.js
@@ -1,1 +1,26 @@
-console.log('Nuevo Ticket HTML');
+// Referencias
+const lblNuevoTicket = document.querySelector('#lblNuevoTicket');
+const btnGenerar = document.querySelector('button');
+
+const socket = io();
+
+socket.on('connect', () => {
+    btnGenerar.disabled = false;
+});
+
+socket.on('disconnect', () => {
+    btnGenerar.disabled = true;
+});
+
+socket.on('currentTicket', (ticket) => {
+    console.log(ticket);
+    lblNuevoTicket.innerText = 'TTUR-' + ticket;
+});
+
+
+btnGenerar.addEventListener('click', () => {
+    socket.emit('nextTicket', null, (ticket) => {
+        console.log('Desde el server', ticket);
+        lblNuevoTicket.innerText = ticket;
+    });
+});
